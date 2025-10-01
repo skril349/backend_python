3700f5611b599baf7d3b749fa998d5b98031f8ec
skril349 <tvives.cabaleiro@gmail.com>
2025-10-01 12:10:52 +0200

    adding try catch to add info on database

--- Django_Flask/proyecto-python/usuarios/usuario.py
+++ Django_Flask/proyecto-python/usuarios/usuario.py
@@ -22,10 +22,16 @@ class Usuario:
         fecha = datetime.datetime.now()
         sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
         usuario = (self.nombre, self.apellidos, self.email, self.password, fecha )
-        cursor.execute(sql,usuario)
-        database.commit()
-        return [cursor.rowcount, self]
-    
+        try:
+            cursor.execute(sql, usuario)
+            database.commit()
+            result = [cursor.rowcount, self]
+            return result
+        except Exception as e:
+            print("Error al registrar usuario:", e)
+            result = [0, self]
+            return result
+
     def identificar(self):
         return self.nombre
 
