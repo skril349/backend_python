be189036e5710f9e27a04dc202e0bf605374cc40
skril349 <tvives.cabaleiro@gmail.com>
2025-10-01 12:15:44 +0200

    adding hast for password

--- Django_Flask/proyecto-python/usuarios/usuario.py
+++ Django_Flask/proyecto-python/usuarios/usuario.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import datetime
+import hashlib
 database = mysql.connector.connect(
     host= "localhost",
     user = "root",
@@ -20,8 +21,11 @@ class Usuario:
     
     def registrar(self):
         fecha = datetime.datetime.now()
+        #cifrar contraseña
+        cifrado = hashlib.sha256()
+        cifrado.update(self.password.encode("utf-8"))
         sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
-        usuario = (self.nombre, self.apellidos, self.email, self.password, fecha )
+        usuario = (self.nombre, self.apellidos, self.email, cifrado.hexdigest(), fecha )
         try:
             cursor.execute(sql, usuario)
             database.commit()
