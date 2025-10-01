956e306e913cebc9540437f926d93cf50b052deb
skril349 <tvives.cabaleiro@gmail.com>
2025-10-01 12:06:42 +0200

    creating users un database

--- Django_Flask/proyecto-python/usuarios/acciones.py
+++ Django_Flask/proyecto-python/usuarios/acciones.py
@@ -1,3 +1,5 @@
+import usuarios.usuario as modelo
+
 class Acciones:
     def registro(self):
         print("\nOk! Vamos a registrarnos en el sistema ...")
@@ -5,6 +7,14 @@ class Acciones:
         apellidos = input("cuales son tus apellidos?")
         email = input("cual es tu email?")
         password = input("introduce tu conrtraseña: ")
+
+        usuario = modelo.Usuario(nombre, apellidos, email, password)
+        registro = usuario.registrar()
+
+        if registro[0] >= 1:
+            print(f"Perfecto {registro[1].nombre}, te has registrado con el email {registro[1].email}")
+        else:
+            print("No te has registrado correctamente")
     
     def login(self):
         print("Vale! Identificate en el sistema ...")
--- Django_Flask/proyecto-python/usuarios/usuario.py
+++ Django_Flask/proyecto-python/usuarios/usuario.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import datetime
 database = mysql.connector.connect(
     host= "localhost",
     user = "root",
@@ -16,10 +16,15 @@ class Usuario:
         self.nombre = nombre
         self.apellidos = apellidos
         self.email = email
-        self.passowrd = password
+        self.password = password
     
     def registrar(self):
-        return self.nombre
+        fecha = datetime.datetime.now()
+        sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
+        usuario = (self.nombre, self.apellidos, self.email, self.password, fecha )
+        cursor.execute(sql,usuario)
+        database.commit()
+        return [cursor.rowcount, self]
     
     def identificar(self):
         return self.nombre
